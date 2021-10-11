e9a47ee81d03584ce5eeaa88bf137fefa9841fd6
Billeclipse <billchelseaaek@hotmail.com>
2021-10-11 20:02:59 +0300

    Lectures 39-43 : Organise the structure of the components

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,6 +1,7 @@
+import React from "react";
 import "./ExpenseDate.css";
 
-function ExpenseDate(props) {
+const ExpenseDate = (props) => {
   const day = props.date.toLocaleString("en-GR", { day: "2-digit" });
   const month = props.date.toLocaleString("en-GR", { month: "long" });
   const year = props.date.getFullYear();
