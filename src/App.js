cf13183a1c5149d1b87dbfc97b4aed1a249b94a1
Billeclipse <billchelseaaek@hotmail.com>
2021-10-17 19:09:15 +0300

    Lectures 66-67: Adding an expenses list.

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ const App = () => {
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses expenses={expenses} />
+      <Expenses items={expenses} />
     </div>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [enteredFilterYear, setEnteredFilterYear] = useState("2020");
@@ -12,25 +12,17 @@ const Expenses = (props) => {
     setEnteredFilterYear(filterYear);
   };
 
+  const filteredExpenses = props.items.filter(
+    (expense) => expense.date.getFullYear().toString() === enteredFilterYear
+  );
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={enteredFilterYear}
         onSaveFilterData={saveFilterDataHandler}
       />
-      {props.expenses
-        .filter(
-          (expense) =>
-            expense.date.getFullYear().toString() === enteredFilterYear
-        )
-        .map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
